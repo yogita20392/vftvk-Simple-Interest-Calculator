c384506d90331c5840c2e959670e05bba0387a90
yogita20392 <77966061+yogita20392@users.noreply.github.com>
2021-05-15 22:05:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function compute()
+
+   function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   var p = document.getElementById("principal").value;
+   if(p<=0)
+   {
+       alert("enter a positive number greater than zero");
+       document.getElementById("prinicipal").focus();
+       document.getElementById("prinicipal").click();
+       return null;
+   }
+   var r = document.getElementById("rate").value;
+   var y =document.getElementById("years").value;
+   console.log(y);
+   var interest = p*r*y/100;
+   console.log(p*r);
+   var ndate = new Date();
+   newdate = ndate.getFullYear();
+   newdate+= parseInt(y);
+   document.getElementById("result").innerHTML =  "<br>" + "<br>" + "If you deposit " + "<span class='highlight'>" + p + '</span>' +'<br>' + ' at an interest rate of ' + "<span class='highlight'>" + r + '</span>' +'%.' +'<br>' + 'You will recive an amount of ' + "<span class='highlight'>" +interest + '</span>' +'<br>' + 'In the year ' + "<span class='highlight'>" +newdate + '</span>';
 }
-        
+function ratechange()
+{
+    var rchng = document.getElementById("rate").value;
+    document.getElementById("rate-displayer").innerHTML = rchng + '%';
+}     
